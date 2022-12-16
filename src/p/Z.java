2136b9fd52dbbc627190b1e6f985fdf10eb5f347
5ashish <ashish.ku.das@gmail.com>
2022-12-16 14:24:47 +0530

    ZX

---
+++ src/p/Z.java
@@ -0,0 +1,14 @@
+package p;
+
+public class Z {
+
+	public Z() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
